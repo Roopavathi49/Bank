475967c2c5a621f4da6efe72460afc3cb1e0ddd3
Roopavathi49 <82867356+Roopavathi49@users.noreply.github.com>
2021-04-27 12:18:26 +0530

    Add files via upload

---
+++ Transaction.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankStaff
+{
+    public class Transaction
+    {
+        private List<Transaction> allTransactions = new List<Transaction>();
+        public decimal Amount { get; }
+        public DateTime Date { get; }
+        public string Notes { get; }
+
+        public Transaction(decimal amount, DateTime date, string note)
+        {
+            this.Amount = amount;
+            this.Date = date;
+            this.Notes = note;
+        }
+        public decimal Balance
+        {
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+
+                return balance;
+            }
+        }
+
+    }
+}
